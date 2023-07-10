b9000e9564bf6428372f9fb436d88c9c472d2eef
joniles <jon.iles@bcs.org.uk>
2023-07-10 16:02:49 +0100

    WIP

---
+++ mpxj.net/proxy/DeepProxyCollection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace org.mpxj.proxy
+{
+    public class DeepProxyCollection<M, N> : DeepProxyEnumerable<N>, ICollection<N>, IJavaObjectProxy<java.util.Collection> where N : IJavaObjectProxy<M>
+    {
+        public new java.util.Collection JavaObject { get => (java.util.Collection)base.JavaObject; }
+
+        public int Count => JavaObject.size();
+
+        public bool IsReadOnly => false;
+
+        internal DeepProxyCollection(ProxyManager proxyManager, java.util.Collection javaObject) : base(proxyManager, javaObject) { }
+
+        public void Add(N item)
+        {
+            JavaObject.add(item.JavaObject);
+        }
+
+        public void Clear()
+        {
+            JavaObject.clear();
+        }
+
+        public bool Contains(N item)
+        {
+            return JavaObject.contains(item.JavaObject);
+        }
+
+        public void CopyTo(N[] array, int arrayIndex)
+        {
+            foreach (N item in this)
+            {
+                array[arrayIndex++] = item;
+            }
+        }
+
+        public bool Remove(N item)
+        {
+            return JavaObject.remove(item.JavaObject);
+        }
+    }
+}
--- mpxj.net/proxy/DeepProxyList.cs
+++ mpxj.net/proxy/DeepProxyList.cs
@@ -1,108 +1,18 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace org.mpxj.proxy
 {
-	public class DeepProxyList<M, N> : IList<N>, IJavaObjectProxy<java.util.List> where N : IJavaObjectProxy<M>
+	public class DeepProxyList<M, N> : DeepProxyCollection<M, N>, IList<N>, IJavaObjectProxy<java.util.List> where N : IJavaObjectProxy<M>
     {
-        public struct Enumerator<ME, NE> : IEnumerator<NE>, IDisposable, IEnumerator where NE : IJavaObjectProxy<ME>
-        {
-            private readonly DeepProxyList<ME, NE> _list;
+        public new java.util.List JavaObject { get => (java.util.List)base.JavaObject; }
 
-            private java.util.Iterator _iter;
-
-            private NE _current;
-
-            public NE Current { get => _current; }
-
-            object IEnumerator.Current
-            {
-                get => _current;
-            }
-
-            internal Enumerator(DeepProxyList<ME, NE> list)
-            {
-                _list = list;
-                _iter = list.JavaObject.iterator();
-                _current = default(NE);
-            }
-
-            public void Dispose()
-            {
-            }
-
-            public bool MoveNext()
-            {
-                if (!_iter.hasNext())
-                {
-                    return false;
-                }
-
-                _current = (NE)_list._proxyManager.GenericProxyObject(_iter.next());
-                return true;
-            }
-
-            public void Reset()
-            {
-                _iter = _list.JavaObject.iterator();
-                _current = default(NE);
-            }
-        }
-
-        internal readonly ProxyManager _proxyManager;
-        public java.util.List JavaObject { get; }
-
-        internal DeepProxyList(ProxyManager proxyManager)
-        {
-            _proxyManager = proxyManager;
-            JavaObject = new java.util.ArrayList();
-        }
-
-        internal DeepProxyList(ProxyManager proxyManager, java.util.List javaObject)
-        {
-            _proxyManager = proxyManager;
-            JavaObject = javaObject;
-        }
+        internal DeepProxyList(ProxyManager proxyManager, java.util.List javaObject) : base(proxyManager, javaObject) { }
 
         public N this[int index]
         {
             get => (N)JavaObject.get(index);
             set => JavaObject.set(index, value);
         }
-
-        public int Count => JavaObject.size();
-
-        public bool IsReadOnly => false;
-
-        public void Add(N item)
-        {
-            JavaObject.add(item.JavaObject);
-        }
-
-        public void Clear()
-        {
-            JavaObject.clear();
-        }
-
-        public bool Contains(N item)
-        {
-            return JavaObject.contains(item.JavaObject);
-        }
-
-        public void CopyTo(N[] array, int arrayIndex)
-        {
-            foreach (N item in this)
-            {
-                array[arrayIndex++] = item;
-            }
-        }
-
-        public IEnumerator<N> GetEnumerator()
-        {
-            return new Enumerator<M, N>(this);
-        }
-
         public int IndexOf(N item)
         {
             return JavaObject.indexOf(item);
@@ -113,19 +23,9 @@ namespace org.mpxj.proxy
             JavaObject.set(index, item.JavaObject);
         }
 
-        public bool Remove(N item)
-        {
-            return JavaObject.remove(item.JavaObject);
-        }
-
         public void RemoveAt(int index)
         {
             JavaObject.remove(index);
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return new Enumerator<M, N>(this);
         }
     }
 }
