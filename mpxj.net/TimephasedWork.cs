279353f8c224030bfef28f4edfb65c001bd2e58f
joniles <jon.iles@bcs.org.uk>
2023-07-12 10:07:54 +0100

    WIP

---
+++ mpxj.net/TimephasedWork.cs
@@ -0,0 +1,15 @@
+﻿using org.mpxj.proxy;
+
+namespace org.mpxj
+{
+	public class TimephasedWork : IJavaObjectProxy<net.sf.mpxj.TimephasedWork>
+    {
+        public net.sf.mpxj.TimephasedWork JavaObject { get; }
+
+        internal TimephasedWork(net.sf.mpxj.TimephasedWork javaObject)
+        {
+            JavaObject = javaObject;
+        }
+	}
+}
+
