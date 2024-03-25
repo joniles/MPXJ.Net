91fb22bbe0cdfa560917af32077305ebc5cccc3d
joniles <jon.iles@bcs.org.uk>
2024-03-25 13:30:17 +0000

    WIP

---
+++ mpxj.net.tests/StepTest.cs
@@ -0,0 +1,30 @@
+﻿
+using NUnit.Framework;
+using org.mpxj.reader;
+
+namespace org.mpxj
+{
+    public class StepTest
+    {
+        [Test]
+        public void MethodTest()
+        {
+            var project = new UniversalProjectReader().Read("TestData/Sample1.xer");
+            Assert.That(project, Is.Not.Null);
+
+            var task = project.Tasks[1];
+            Assert.That(task.Steps, Has.Count.EqualTo(2));
+
+            var step = task.Steps[0];
+            Assert.That(step.Complete, Is.True);
+            Assert.That(step.Description, Is.EqualTo("Step 1 notes"));
+            Assert.That(step.DescriptionObject.ToString(), Contains.Substring("Step 1 notes"));
+            Assert.That(step.Name, Is.EqualTo("Step 1"));
+            Assert.That(step.PercentComplete, Is.EqualTo(100.0));
+            Assert.That(step.SequenceNumber, Is.EqualTo(0));
+            Assert.That(step.Task, Is.EqualTo(task));
+            Assert.That(step.UniqueID, Is.EqualTo(5743));
+            Assert.That(step.Weight, Is.EqualTo(1));
+        }
+    }
+}
