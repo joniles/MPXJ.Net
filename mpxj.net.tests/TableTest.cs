35df2fa6fc52ffb316c035259b14daa9a7d369fe
joniles <jon.iles@bcs.org.uk>
2024-03-25 13:51:17 +0000

    WIP

--- mpxj.net.tests/TableTest.cs
+++ mpxj.net.tests/TableTest.cs
@@ -13,6 +13,7 @@ namespace org.mpxj
             Assert.That(project, Is.Not.Null);
 
             Assert.That(project.Tables, Has.Count.EqualTo(3));
+            Assert.That(project.Tables.JavaObject, Is.Not.Null);
 
             var table = project.Tables[0];
             Assert.That(table.ID, Is.EqualTo(1));
@@ -29,6 +30,10 @@ namespace org.mpxj
             Assert.That(column.Title, Is.EqualTo("ID"));
             Assert.That(column.Width, Is.EqualTo(6));
             Assert.That(column.ToString(), Contains.Substring("[Column type=ID width=6"));
+
+            Assert.That(project.Tables.GetTaskTableByName("Entry"), Is.Not.Null);
+            Assert.That(project.Tables.GetResourceTableByName("Entry"), Is.Not.Null);
+
         }
     }
 }
--- mpxj.net.tests/UniversalProjectReaderTest.cs
+++ mpxj.net.tests/UniversalProjectReaderTest.cs
@@ -1,5 +1,6 @@
 ﻿using org.mpxj.reader;
 using NUnit.Framework;
+using System.IO;
 
 namespace org.mpxj
 {
@@ -10,8 +11,24 @@ namespace org.mpxj
         {
             var project = new UniversalProjectReader().Read("TestData/Sample1.mpp");
             Assert.That(project, Is.Not.Null);
+
             var config = project.ProjectConfig;
             Assert.That(config, Is.Not.Null);
+
+            var projects = new UniversalProjectReader().ReadAll("TestData/Sample1.xml");
+            Assert.That(projects, Has.Count.EqualTo(1));
+
+            using (var stream1 = new FileStream("TestData/Sample1.mpp", FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                project = new UniversalProjectReader().Read(stream1);
+                Assert.That(project, Is.Not.Null);
+            }
+
+            using (var stream2 = new FileStream("TestData/Sample1.xer", FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                projects = new UniversalProjectReader().ReadAll(stream2);
+                Assert.That(projects, Has.Count.EqualTo(1));
+            }
         }
     }
 }
