785c81234acfc12954a4b4180e8d0a49511aa62b
joniles <jon.iles@bcs.org.uk>
2024-04-15 15:30:28 +0100

    WIP

--- mpxj.net.tests/PriorityTest.cs
+++ mpxj.net.tests/PriorityTest.cs
@@ -10,6 +10,18 @@ namespace org.mpxj
             var priority = Priority.GetInstance(123);
             Assert.That(priority.Value, Is.EqualTo(123));
             Assert.That(priority.ToString(), Is.EqualTo("[Priority value=123]"));
+
+            // Coverage
+            Assert.That(Priority.Lowest, Is.EqualTo(100));
+            Assert.That(Priority.VeryLow, Is.EqualTo(200));
+            Assert.That(Priority.Lower, Is.EqualTo(300));
+            Assert.That(Priority.Low, Is.EqualTo(400));
+            Assert.That(Priority.Medium, Is.EqualTo(500));
+            Assert.That(Priority.High, Is.EqualTo(600));
+            Assert.That(Priority.Higher, Is.EqualTo(700));
+            Assert.That(Priority.VeryHigh, Is.EqualTo(800));
+            Assert.That(Priority.Highest, Is.EqualTo(900));
+            Assert.That(Priority.DoNotLevel, Is.EqualTo(1000));
         }
     }
 }
