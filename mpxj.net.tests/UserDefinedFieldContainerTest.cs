2c2bdd8cdfe94fd1ea1150a0e02454d019eb81ec
joniles <jon.iles@bcs.org.uk>
2024-04-15 12:48:09 +0100

    WIP

---
+++ mpxj.net.tests/UserDefinedFieldContainerTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using org.mpxj.reader;
+
+namespace org.mpxj
+{
+    public class UserDefinedFieldContainerTest
+    {
+        [Test]
+        public void MethodTests()
+        {
+            var project = new UniversalProjectReader().Read("TestData/Sample1.xer");
+            Assert.That(project, Is.Not.Null);
+
+            Assert.That(project.UserDefinedFields.TaskFields, Has.Count.EqualTo(2));
+            Assert.That(project.UserDefinedFields.ResourceFields, Has.Count.EqualTo(0));
+            Assert.That(project.UserDefinedFields.AssignmentFields, Has.Count.EqualTo(0));
+            Assert.That(project.UserDefinedFields.ProjectFields, Has.Count.EqualTo(0));
+        }
+    }
+}
