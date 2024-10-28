1c3d8416481875b9e1e765c056d4294d4c2c0414
sza <sza@home>
2024-10-28 15:04:48 +0300

    Add UniversalProjectWriter tests

---
+++ mpxj.net.tests/UniversalProjectWriterTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+namespace MPXJ.Net
+{
+	public class UniversalProjectWriterTests
+	{
+		private const string Path = "TestData/Results/";
+
+		[SetUp]
+		public void SetUp()
+		{
+			Directory.CreateDirectory(Path);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			Directory.Delete(Path, true);
+		}
+
+		[Test]
+		public void ProjectWriter_can_write_to_file()
+		{
+			var project = new UniversalProjectReader().Read("TestData/Sample1.mpp");
+			var filePath = Path + "Sample1-written.xml";
+
+			new UniversalProjectWriter(FileFormat.MSPDI).Write(project, filePath);
+
+			Assert.That(File.Exists(filePath), Is.True);
+		}
+
+		[Test]
+		public void ProjectWriter_can_write_to_stream()
+		{
+			using (var ms = new MemoryStream())
+			{
+				var project = new UniversalProjectReader().Read("TestData/Sample1.mpp");
+				var filePath = Path + "Sample1-written-from-stream.xml";
+
+				new UniversalProjectWriter(FileFormat.MSPDI).Write(project, ms);
+
+				File.WriteAllBytes(filePath, ms.ToArray());
+				Assert.That(File.Exists(filePath), Is.True);
+			}
+		}
+	}
+}
