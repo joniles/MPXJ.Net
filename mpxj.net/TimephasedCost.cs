32c8db631c3eb5be37d7942bf8055f159bf7638c
joniles <jon@timephased.com>
2025-01-29 17:47:00 +0000

    Timephased Item Updates

--- mpxj.net/TimephasedCost.cs
+++ mpxj.net/TimephasedCost.cs
@@ -2,12 +2,24 @@
 
 namespace MPXJ.Net
 {
-    public class TimephasedCost : TimephasedItem<java.lang.Number>, IJavaObjectProxy<net.sf.mpxj.TimephasedCost>
+    public class TimephasedCost : TimephasedItem<double?>, IJavaObjectProxy<net.sf.mpxj.TimephasedCost>
     {
         public new net.sf.mpxj.TimephasedCost JavaObject => (net.sf.mpxj.TimephasedCost)base.JavaObject;
 
         internal TimephasedCost(net.sf.mpxj.TimephasedCost javaObject) : base(javaObject) { }
 
         public TimephasedCost() : base(new net.sf.mpxj.TimephasedCost()) { }
+        
+        public override double? TotalAmount
+        {
+            get => ((java.lang.Number)JavaObject.getTotalAmount()).ConvertType();
+            set => JavaObject.setTotalAmount(value.ConvertType());
+        }
+        
+        public override double? AmountPerDay
+        {
+            get => ((java.lang.Number)JavaObject.getAmountPerDay()).ConvertType();
+            set => JavaObject.setAmountPerDay(value.ConvertType());
+        }
     }
 }
