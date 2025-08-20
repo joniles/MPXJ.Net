a63be1df114ada5f5d6803776e565b91b3001551
Jon Iles <jon.iles@bcs.org.uk>
2025-08-20 11:15:32 +0100

    Add missing reader proxies (#29)

---
+++ mpxj.net/EdrawProjectReader.cs
@@ -0,0 +1,11 @@
+﻿namespace MPXJ.Net
+{
+    public class EdrawProjectReader : AbstractProjectReader
+    {
+        internal EdrawProjectReader(org.mpxj.edrawproject.EdrawProjectReader reader) : base(reader) { }
+
+        public EdrawProjectReader() : base(new org.mpxj.edrawproject.EdrawProjectReader()) { }
+
+        public new org.mpxj.edrawproject.EdrawProjectReader JavaObject => (org.mpxj.edrawproject.EdrawProjectReader)base.JavaObject;
+    }
+}
